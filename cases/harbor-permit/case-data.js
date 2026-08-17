1c037bc63ac707562b0b6575fc0d597be5d65982
coldysquares <nickberghane@gmail.com>
2026-08-16 22:21:22 -0400

    Load modular ChronosAudit case data in order

--- cases/harbor-permit/case-data.js
+++ cases/harbor-permit/case-data.js
@@ -1,8 +1,8 @@
-window.CHRONOS_CASE = {
-  ...window.CHRONOS_CASE_CORE,
-  sources: window.CHRONOS_CASE_SOURCES || [],
-  assertions: window.CHRONOS_CASE_ASSERTIONS || [],
-  events: window.CHRONOS_CASE_EVENTS || [],
-  contradictions: window.CHRONOS_CASE_FLAGS?.contradictions || [],
-  anomalies: window.CHRONOS_CASE_FLAGS?.anomalies || []
-};
+(() => {
+  "use strict";
+  const base = "./cases/harbor-permit/data/";
+  for (const file of ["core.js", "sources.js", "assertions.js", "events.js", "flags.js"]) {
+    document.write(`<script src="${base}${file}"><\/script>`);
+  }
+  document.write(`<script>window.CHRONOS_CASE={...window.CHRONOS_CASE_CORE,sources:window.CHRONOS_CASE_SOURCES||[],assertions:window.CHRONOS_CASE_ASSERTIONS||[],events:window.CHRONOS_CASE_EVENTS||[],contradictions:window.CHRONOS_CASE_FLAGS?.contradictions||[],anomalies:window.CHRONOS_CASE_FLAGS?.anomalies||[]};<\/script>`);
+})();
